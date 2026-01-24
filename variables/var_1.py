a32e7337601ac16a3bead7f263868b665360314e
murs-leen <zahidmursleen004@gmail.com>
2026-01-24 15:21:16 +0500

    variable identification

--- variables/var_1.py
+++ variables/var_1.py
@@ -1,2 +1,17 @@
-print("This is my first program in python")
-print("This is line 1","This is line 2")
+
+#variables
+# no data types are required for identifiers 
+# it depends on stored data
+
+name = "mursleen" # string 
+age = 21 # integer
+price = 25.565 # float
+
+print ("my name is : ", name)
+print ("my age is : ", age)
+
+# types
+
+print(type(name))
+print(type(age))
+print(type(price))
