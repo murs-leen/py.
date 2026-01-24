0109b8ade6bf5b4ae3f5623e0274fdaa166a2264
murs-leen <zahidmursleen004@gmail.com>
2026-01-25 00:37:00 +0500

    string operations and methods

---
+++ strings/string_methods.py
@@ -0,0 +1,14 @@
+
+#   String Methods
+
+str = "learning python from Youtube"
+
+print("String ends with: (ube)",str.endswith("ube")) #    return (true/false)
+
+print("Capitalize frist char:",str.capitalize()) #   capitalize 1st character and returns copy
+
+str = str.replace("e","0") #    returns copy
+print("Replace e with 0",str)
+
+print("Finding 'f' in str: ",str.find("f"))
+
---
+++ strings/string_operations.py
@@ -0,0 +1,17 @@
+ 
+#   String operations
+
+str1 = "This is string of type1"
+str2 = 'This is string of type2'
+str3 = """This is string of type3"""
+
+print(str1 + str2) #    concatination
+print(len(str1)) #  lenght
+
+#   -ve indexing is valid in python [-1] last character ....[-(size + 1)]
+
+print(str1[-4])
+
+#   Slicing
+
+print(str3[0:4]) #  [ )
