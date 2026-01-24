3794a9f070a9d240487fb800461e3f7699a0b1ab
murs-leen <zahidmursleen004@gmail.com>
2026-01-24 21:51:12 +0500

    operators

---
+++ operators/types_of_operator.py
@@ -0,0 +1,19 @@
+
+#   Arithematic operator
+
+x = 7
+y  = 2
+
+print("mul: ",x * y)
+print("add: ",x + y)
+print("sub: ",x - y)
+print("divide: ",x / y)  
+print(x % y)
+
+
+#   Relational operator
+
+if x and y:
+    print("hi")
+
+
