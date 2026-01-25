19f6e5d40adf11673e760877fb152460b7c0c057
murs-leen <zahidmursleen004@gmail.com>
2026-01-25 16:45:12 +0500

    intit commit

---
+++ Dic/dic.py
@@ -0,0 +1,10 @@
+
+dic = {
+    "name" : "zahid-mursleen",
+    "age" : 21,
+    "code" : "A5B3"
+}
+
+print(dic.items()) #    reutrn key-value pair
+print(dic.keys()) #     return keys of dic
+print(dic.values()) #   return values
